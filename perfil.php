df9b3de3ff1b3bc284ec3c5d9198513d29a47607
dajocordo <42018108+dajocordo@users.noreply.github.com>
2021-06-29 10:46:34 -0600

    Add files via upload

--- perfil.php
+++ perfil.php
@@ -1,217 +1,214 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Perfil</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <!-- Bootstrap CSS -->
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Perfil</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-        <style type="text/css">
+    <style type="text/css">
 
-        body{
-            background-color: #22293D;
-            color: white;
-            font-family: 'Open Sans', sans-serif;
-        }
+      body{
+        background-color: #22293D;
+        color: white;
+        font-family: 'Open Sans', sans-serif;
+      }
 
-        .container{
-            text-align: center;
-            justify-content: center;
-            align-items: center;
-        }
-  
-        .seeprofile {
-            margin: 0.9em;
-            height: 4em;
-            width: 25%;
-            font-weight: bold;
-            color: #22293D;
-            display: inline-block;
-            background-color: #ddd;
-        }
-  
-        .editprofile {
-            margin: 0.9em;
-            height: 4em;
-            width: 25%;
-            font-weight: bold;
-            color: #22293D;
-            display: inline-block;
-            background-color: #ddd;
-        }
-  
-        .knowteam {
-            margin: 0.9em;
-            height: 4em;
-            width: 25%;
-            font-weight: bold;
-            color: #22293D;
-            display: inline-block;
-            background-color: #ddd;
-        }
+      .container{
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+      }
+      
+      .seeprofile {
+        margin: 0.9em;
+        height: 4em;
+        width: 25%;
+        font-weight: bold;
+        font-size: 1.25em;
+        color: #22293D;
+        display: inline-block;
+        background-color: #FFCCBC;
+      }
+      
+      .editprofile {
+        margin: 0.9em;
+        height: 4em;
+        width: 25%;
+        font-weight: bold;
+        font-size: 1.25em;
+        color: #22293D;
+        display: inline-block;
+        background-color: #B2EBF2;
+      }
+      
+      .knowteam {
+        margin: 0.9em;
+        height: 4em;
+        width: 25%;
+        font-weight: bold;
+        font-size: 1.25em;
+        color: #22293D;
+        display: inline-block;
+        background-color: #DCEDC8;
+      }
+      
+      #greeting{
+        margin-top: 1em;
+        margin-bottom: 1em;
+      }
+      
+      .ppro{ padding-top: 1.2em;}
 
-        #greeting{
-            margin-top: 1em;
-            margin-bottom: 1em;
+      .modal-body, .modal-title {
+        color: #22293D;
+        font-weight: bold;            
+      }
+      
+      .btn-primary{ background-color: #22293D;}
+       
+      .topnav {
+        overflow: hidden;
+        background-color: #22293D;
+        font-weight: bold;
+      }
+      
+      .topnav a {
+        margin-top: 0.7em;
+        margin-left: 0.7em;
+        float: left;
+        display: block;
+        color: #f2f2f2;
+        text-align: center;
+        padding-top: 0.7em;
+        padding-bottom: 0.7em;
+        padding-left: 2.15em;
+        padding-right: 2.15em;
+        text-decoration: none;
+        font-size: 1.2em;
+      }
+      
+      .topnav a:hover {
+        background-color: #ddd;
+        color: black;
+      }
+      
+      .topnav a.active {
+        background-color: #ddd;
+        color: #22293D;
+      }
+      
+      .topnav .icon {
+        display: none;
+      }
+      
+      .topnav .tophome {
+        margin-left: 0.7em;
+      }
+      
+      @media screen and (max-width: 600px) {
+        .topnav a:not(:first-child) {display: none;}
+        .topnav a.icon {
+          float: right;
+          display: block;
         }
-
-        .ppro{
-            padding-top: 1.2em;
+      }
+      
+      @media screen and (max-width: 600px) {
+        .topnav.responsive {position: relative;}
+        .topnav.responsive .icon {
+          position: absolute;
+          right: 0;
+          top: 0;
         }
-
-        .modal-body, .modal-title {
-            color: #22293D;
-            font-weight: bold;            
+        .topnav.responsive a {
+          float: none;
+          display: block;
+          text-align: left;
         }
-
-        .btn-primary{
-            background-color: #22293D;
-        }
-
-        .topnav {
-            overflow: hidden;
-            background-color: #22293D;
-            font-weight: bold;
-        }
-
-        .topnav a {
-            margin-top: 0.7em;
-            margin-left: 0.7em;
-            float: left;
-            display: block;
-            color: #f2f2f2;
-            text-align: center;
-            padding-top: 0.7em;
-            padding-bottom: 0.7em;
-            padding-left: 2.15em;
-            padding-right: 2.15em;
-            text-decoration: none;
-            font-size: 1.2em;
-        }
-
-        .topnav a:hover {
-          background-color: #ddd;
-          color: black;
-        }
-
-        .topnav a.active {
-          background-color: #ddd;
-          color: #22293D;
-        }
-
-        .topnav .icon {
-          display: none;
-        }
-
-        .topnav .tophome {
-            margin-left: 0.7em;
-        }
-
-        @media screen and (max-width: 600px) {
-          .topnav a:not(:first-child) {display: none;}
-          .topnav a.icon {
-            float: right;
-            display: block;
-          }
-        }
-
-        @media screen and (max-width: 600px) {
-          .topnav.responsive {position: relative;}
-          .topnav.responsive .icon {
-            position: absolute;
-            right: 0;
-            top: 0;
-          }
-          .topnav.responsive a {
-            float: none;
-            display: block;
-            text-align: left;
-          }
-        }
+      }
           
-        @media screen and (max-width: 600px) {
-            .seeprofile,
-            .editprofile,
-            .knowteam {
-                width: 70%;
-                height: 5em;
-            }
-            .ppro{
-                padding-top: 1.7em;
-            }
+      @media screen and (max-width: 600px) {
+        .seeprofile,
+        .editprofile,
+        .knowteam {
+          width: 70%;
+          height: 5em;
         }
-        </style>
+        .ppro{
+          padding-top: 1.7em;
+        }
+      }
+      </style>
     </head>
     <body>
 
-        <!--|==========| Barra de navegacion | ↓ | inicio |==========|-->
-        <div class="topnav" id="myTopnav">
-            <a href="home.php">Inicio</a>
-            <a href="notas.php">Notas</a>
-            <a href="perfil.php" class="active">Perfil</a>
-            <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir" data-bs-target="#CerrarSesion">Salir</a>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-              <i class="fa fa-bars"></i>
-            </a>
-        </div>
-        <script>
-            function myFunction() {
-              var x = document.getElementById("myTopnav");
-              if (x.className === "topnav") {
-                x.className += " responsive";
-              } else {
-                x.className = "topnav";
-              }
-            }
+      <!--|==========| Barra de navegacion | ↓ | inicio |==========|-->
+      <div class="topnav" id="myTopnav">
+        <a href="home.php">Inicio</a>
+        <a href="notas.php">Notas</a>
+        <a href="perfil.php" class="active">Perfil</a>
+        <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir" data-bs-target="#CerrarSesion">Salir</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+          <i class="fa fa-bars"></i>
+        </a>
+      </div>
+      <script>
+        function myFunction() {
+          var x = document.getElementById("myTopnav");
+          if (x.className === "topnav") {
+            x.className += " responsive";
+          } else {
+            x.className = "topnav";
+          }
+        }
         </script>
         <!--|==========| Barra de navegacion | ↑ | fin |==========|-->
 
         <!--|==========| Container | ↓ | inicio |==========|-->
         <div class="container">
-            <!--|==========| Perfil | ↓ |==========|-->
-            <h1 id="greeting">Perfil</h1>
-
-            <!--|==========| Ver perfil | ↓ |==========|-->
-            <div class="seeprofile">
-                <p class="ppro">Ver perfil</p>
-            </div>
-            <!--|==========| Editar perfil | ↓ |==========|-->
-            <div class="editprofile">
-                <p class="ppro">Editar perfil</p>
-            </div>
-            <!--|==========| Conocer equipo | ↓ |==========|-->
-            <div class="knowteam">
-                <p class="ppro">Conocer equipo</p>
-            </div>
+          <!--|==========| Perfil | ↓ |==========|-->
+          <h1 id="greeting">Perfil</h1>
+          <!--|==========| Ver perfil | ↓ |==========|-->
+          <div class="seeprofile">
+            <p class="ppro">Mi cuenta</p>
+          </div>
+          <!--|==========| Editar perfil | ↓ |==========|-->
+          <div class="editprofile">
+            <p class="ppro">Editar perfil</p>
+          </div>
+          <!--|==========| Conocer equipo | ↓ |==========|-->
+          <div class="knowteam">
+            <p class="ppro">Mi equipo</p>
+          </div>
         </div>
         <!--|==========| Container | fin | ↑ |==========|-->
 
-        <!--|==========| Modal | inicio | ↓ | Cerrar Sesion |==========|-->
-        <div class="modal fade" id="CerrarSesion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Cerrar Sesion</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                ¿Desea salir de la plataforma?
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-                <a href="welcome.blade.php" type="button" class="btn btn-primary">Si</a>
-              </div>
-            </div>
+    <!--|==========| Modal | inicio | ↓ | Cerrar Sesion |==========|-->
+    <div class="modal fade" id="CerrarSesion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Cerrar Sesion</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ¿Desea salir de la plataforma?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+            <a href="welcome.blade.php" type="button" class="btn btn-primary">Si</a>
           </div>
         </div>
-        <!--|==========| Modal | fin | ↓ | Cerrar Sesion |==========|-->
-
-    </body>
+      </div>
+    </div>
+    <!--|==========| Modal | fin | ↓ | Cerrar Sesion |==========|-->
+  </body>
 </html>
