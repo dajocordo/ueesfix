78d254f1750ce644dba353a23f07137f8402fdfc
MelvinErazo <35872974+MelvinErazo@users.noreply.github.com>
2021-07-07 00:36:45 -0600

    Add files via upload

---
+++ ver_reportes.php
@@ -0,0 +1,513 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inicio</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.js"></script>
+
+
+
+    <style type="text/css">
+        body{
+            background-color: #22293D;
+            color: white;
+            font-family: 'Open Sans', sans-serif;
+        }
+        .container{
+            text-align: center;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .first {
+            text-align: center;
+            margin: 0.9em;
+            height: 4em;
+            width: 25%;
+            font-weight: bold;
+            font-size: 1.25em;
+            color: #22293D;
+            display: inline-block;
+            background-color: rgba(255, 180, 95, 0.85);
+        }
+
+        .second {
+            margin: 0.9em;
+            height: 4em;
+            width: 25%;
+            font-weight: bold;
+            font-size: 1.25em;
+            color: #22293D;
+            display: inline-block;
+            background-color: #FFF176;
+        }
+
+        .third {
+            margin: 0.9em;
+            height: 4em;
+            width: 25%;
+            font-weight: bold;
+            font-size: 1.25em;
+            color: #22293D;
+            display: inline-block;
+            background-color: #81D4FA;
+        }
+        #greeting{
+            margin-top: 1em;
+            margin-bottom: 1em;
+        }
+        .ppro{
+            padding-top: 1.2em;
+        }
+        .modal-body, .modal-title {
+            color: #22293D;
+            font-weight: bold;
+        }
+       
+        .btn-primary{
+            background-color: #CFD7FF;
+            font-weight: bold;
+            font-size: 1.25em;
+            margin-right: 1em;
+            margin-bottom: 1em !important;
+            border: 5em;
+            width: 20em;
+            height: 5em;
+            color: #22293D;
+        }
+
+        .btn-secondaryy{
+            background-color: #3A3B3B;
+            color: #FFFFFF;
+        }
+        .topnav {
+            overflow: hidden;
+            background-color: #22293D;
+            font-weight: bold;
+        }
+        .topnav a {
+            margin-top: 0.7em;
+            margin-left: 0.7em;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding-top: 0.7em;
+            padding-bottom: 0.7em;
+            padding-left: 2.15em;
+            padding-right: 2.15em;
+            text-decoration: none;
+            font-size: 1.2em;
+        }
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+        .topnav a.active {
+            background-color: #ddd;
+            color: #22293D;
+        }
+        .topnav .icon {
+            display: none;
+        }
+
+        .tablareporte {
+            background-color: #E6E6FA;
+            color: #000000;
+            font-weight: bold;
+        }
+        
+        .admintable {
+            background-color: #E6E6FA;
+            color: #000000;
+
+        }
+
+
+
+        @media screen and (max-width: 600px) {
+            .topnav a:not(:first-child) {display: none;}
+            .topnav a.icon {
+                float: right;
+                display: block;
+            }
+        }
+        @media screen and (max-width: 600px) {
+            .topnav.responsive {position: relative;}
+            .topnav.responsive .icon {
+                position: absolute;
+                right: 0;
+                top: 0;
+            }
+            .topnav.responsive a {
+                float: none;
+                display: block;
+                text-align: left;
+            }
+        }
+
+        @media screen and (max-width: 600px) {
+            .first,
+            .second,
+            .third {
+                width: 70%;
+                height: 5em;
+            }
+            .ppro{
+                padding-top: 1.7em;
+            }
+        }
+
+    </style>
+</head>
+<body>
+<!--|==========| Barra de navegacion | ↓ | inicio |==========|-->
+<div class="topnav" id="myTopnav">
+    <a href="home.php" class="active">Inicio</a>
+    <a href="notas.php">Notas</a>
+    <a href="perfil.php">Perfil</a>
+    <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir">Salir</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+    </a>
+</div>
+<script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+</script>
+<!--|==========| Barra de navegacion | ↑ | fin |==========|-->
+<!--|==========| Container | ↓ | inicio |==========|-->
+<div class="container">
+    <!--|==========| Bienvenida | ↓ |==========|-->
+    <h1 id="greeting">Reportes UEES-FIX</h1>
+    <!--|==========| Nuevo | ↓ |==========|-->
+    
+
+<div>
+    <canvas class="tablareporte responsive" id="myChart" width="400" height="150"></canvas>
+<script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Incidencias Registradas', 'Por Hacer', 'En Proceso', 'Hecho', ],
+        datasets: [{
+            label: '#Grafica Incidencias',
+            data: [100, 19, 3, 5],
+            backgroundColor: [
+                'rgb(244, 164, 96)',
+                'rgb(255, 215, 0)',
+                'rgb(65, 105, 225)',
+                'rgb(50, 205, 50)'
+            ],
+            borderColor: [
+                'rgb(245, 222, 179)',
+                'rgb(240, 230, 140)',
+                'rgb(173, 216, 230)',
+                'rgb(152, 251, 152)'
+            ],
+            borderWidth: 3
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
+</div><br>
+
+<div class="admintable">
+<script>
+$(document).ready(function() {
+    $('#tableadm').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
+</script>
+
+
+
+
+
+<table class="admintable" id="tableadm" class="display nowrap" style="width:100%">
+        <thead>
+        <br>
+            <tr>
+                <th>ID</th>
+                <th>Gestion</th>
+                <th>Categoria</th>
+                <th>estado</th>
+                <th>Fecha Ingreso</th>
+                <th>Fecha Fin</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>001</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>002</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>003</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>004</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>005</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>006</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>007</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>008</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>009</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>010</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>002</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>003</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>004</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>005</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>006</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>007</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>008</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>009</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+            <tr>
+                <td>010</td>
+                <td>Mantenimiento PC</td>
+                <td>CPU</td>
+                <td>En Proceso</td>
+                <td>05/07/21</td>
+                <td>06/07/21</td>
+            </tr>
+        </tfoot>
+    </table>
+
+</div>
+
+<div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--|==========| Container | fin | ↑ |==========|-->
+<!--|==========| Modal | inicio | ↓ | Cerrar Sesion |==========|-->
+<div class="modal fade" id="CerrarSesion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Cerrar Sesion</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ¿Desea salir de la plataforma?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                <a href="welcome.blade.php" type="button" class="btn btn-primary">Si</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
+
+<!--|==========| Modal | fin | ↓ | Cerrar Sesion |==========|-->
+</body>
+</html>
---
+++ ver_ticket.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ver Tickets</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+    <style type="text/css">
+        body{
+            background-color: #22293D;
+            color: white;
+            font-family: 'Open Sans', sans-serif;
+        }
+        .container{
+            text-align: center;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .first {
+            text-align: center;
+            margin: 0.9em;
+            height: 4em;
+            width: 25%;
+            font-weight: bold;
+            font-size: 1.25em;
+            color: #22293D;
+            display: inline-block;
+            background-color: rgba(255, 180, 95, 0.85);
+        }
+
+        .second {
+            margin: 0.9em;
+            height: 4em;
+            width: 25%;
+            font-weight: bold;
+            font-size: 1.25em;
+            color: #22293D;
+            display: inline-block;
+            background-color: #FFF176;
+        }
+
+        .third {
+            margin: 0.9em;
+            height: 4em;
+            width: 25%;
+            font-weight: bold;
+            font-size: 1.25em;
+            color: #22293D;
+            display: inline-block;
+            background-color: #81D4FA;
+        }
+        #greeting{
+            margin-top: 1em;
+            margin-bottom: 1em;
+        }
+        .ppro{
+            padding-top: 1.2em;
+        }
+        .modal-body, .modal-title {
+            color: #22293D;
+            font-weight: bold;
+        }
+       
+
+        .card-header{
+            background-color: #CFD7FF;
+            color: #000000;
+            font-weight: bold;
+        }
+
+        .card-body{
+            background-color: #CFD7FF;
+            
+           
+        }
+
+        
+        .btn-primary{
+            background-color: #100086;
+            font-weight: bold;
+            font-size: 1.25em;
+            margin-right: 1em;
+            margin-bottom: 1em !important;
+            border: 5em;
+            width: 20em;
+            height: 5em;
+        }
+
+        .btn-secondaryy{
+            background-color: #3A3B3B;
+            color: #FFFFFF;
+        }
+        .topnav {
+            overflow: hidden;
+            background-color: #22293D;
+            font-weight: bold;
+        }
+        .topnav a {
+            margin-top: 0.7em;
+            margin-left: 0.7em;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding-top: 0.7em;
+            padding-bottom: 0.7em;
+            padding-left: 2.15em;
+            padding-right: 2.15em;
+            text-decoration: none;
+            font-size: 1.2em;
+        }
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+        .topnav a.active {
+            background-color: #ddd;
+            color: #22293D;
+        }
+        .topnav .icon {
+            display: none;
+        }
+        @media screen and (max-width: 600px) {
+            .topnav a:not(:first-child) {display: none;}
+            .topnav a.icon {
+                float: right;
+                display: block;
+            }
+        }
+        @media screen and (max-width: 600px) {
+            .topnav.responsive {position: relative;}
+            .topnav.responsive .icon {
+                position: absolute;
+                right: 0;
+                top: 0;
+            }
+            .topnav.responsive a {
+                float: none;
+                display: block;
+                text-align: left;
+            }
+        }
+
+        @media screen and (max-width: 600px) {
+            .first,
+            .second,
+            .third {
+                width: 70%;
+                height: 5em;
+            }
+            .ppro{
+                padding-top: 1.7em;
+            }
+        }
+
+        td.details-control {
+    background: url('../resources/details_open.png') no-repeat center center;
+    cursor: pointer;
+}
+tr.shown td.details-control {
+    background: url('../resources/details_close.png') no-repeat center center;
+}
+    </style>
+</head>
+<body>
+<!--|==========| Barra de navegacion | ↓ | inicio |==========|-->
+<div class="topnav" id="myTopnav">
+    <a href="home.php" class="active">Inicio</a>
+    <a href="notas.php">Notas</a>
+    <a href="perfil.php">Perfil</a>
+    <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir">Salir</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+    </a>
+</div>
+<script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+</script>
+<!--|==========| Barra de navegacion | ↑ | fin |==========|-->
+<!--|==========| Container | ↓ | inicio |==========|-->
+<div class="container">
+    <!--|==========| Bienvenida | ↓ |==========|-->
+    <h1 id="greeting">Bienvenido UEES-FIX</h1>
+    <!--|==========| Nuevo | ↓ |==========|-->
+
+
+    <div class="card-header">
+                <h3 class="card-title">Gestión de Tickets</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="table-responsive card-body">
+                <table class="table table-bordered">
+                  <thead>                  
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>ID</th>
+                      <th>Categoria</th>
+                      <th>Notas</th>
+                      <th>Progress</th>
+                      <th style="width: 40px">Label</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>001</td>
+                      <td>Administración</td>
+                      <td>Actualización de Software</td>
+                      <td>
+                        <div class="progress progress-xs">
+                          <div class="progress-bar progress-bar-danger" style="width: 100%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-danger">100%</span></td>
+                    </tr>
+                    <tr>
+                      <td>2.</td>
+                      <td>002</td>
+                      <td>Ingenieria</td>
+                      <td>Cambio CPU</td>
+                      <td>
+                        <div class="progress progress-xs">
+                          <div class="progress-bar bg-warning" style="width: 70%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-warning">70%</span></td>
+                    </tr>
+                    <tr>
+                      <td>3.</td>
+                      <td>003</td>
+                      <td>Medicina</td>
+                      <td>Poryector Dañado</td>
+                      <td>
+                        <div class="progress progress-xs progress-striped active">
+                          <div class="progress-bar bg-primary" style="width: 30%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-primary">30%</span></td>
+                    </tr>
+                    <tr>
+                      <td>4.</td>
+                      <td>004</td>
+                      <td>Colecturia</td>
+                      <td>Cambio de teclado</td>
+                      <td>
+                        <div class="progress progress-xs progress-striped active">
+                          <div class="progress-bar bg-success" style="width: 90%"></div>
+                        </div>
+                      </td>
+                      <td><span class="badge bg-success">90%</span></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <ul class="pagination pagination-sm m-0 float-right">
+                  <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">4</a></li>
+                  <li class="page-item"><a class="page-link" href="#">5</a></li>
+                  <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                </ul>
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+<!--|==========| Container | fin | ↑ |==========|-->
+<!--|==========| Modal | inicio | ↓ | Cerrar Sesion |==========|-->
+<div class="modal fade" id="CerrarSesion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Cerrar Sesion</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ¿Desea salir de la plataforma?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                <a href="welcome.blade.php" type="button" class="btn btn-primary">Si</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!--|==========| Modal | fin | ↓ | Cerrar Sesion |==========|-->
+
+<!-- jQuery, Popper.js, Bootstrap JS -->
+<script src="jquery/jquery-3.3.1.min.js"></script>
+    <script src="popper/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+      
+    <!-- datatables JS -->
+    <script type="text/javascript" src="datatables/datatables.min.js"></script>    
+     
+    <script type="text/javascript" src="main.js"></script>  
+    
+</body>
+</html>
