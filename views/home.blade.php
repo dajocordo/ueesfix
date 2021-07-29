27d8243ed29e953cc518d01d19b59ddc87cd48ff
dajocordo <42018108+dajocordo@users.noreply.github.com>
2021-07-28 22:40:59 -0600

    Add files via upload

--- views/home.blade.php
+++ views/home.blade.php
@@ -25,13 +25,39 @@
 
   <!--|====| Container | ↓ | → | inicio |====|--><div class="container">
     <!--|==========| Bienvenida | ↓ |==========|-->
-    <h1 id="greeting">Bienvenido crack</h1>
-    <!--|==========| Nuevo | ↓ |==========|-->
-    <div class="first"> <p class="ppro"> <a href="todo" class="a1">Nuevo</a> </p> </div>
-    <!--|==========| Pendiente | ↓ |==========|-->
-    <div class="second"> <p class="ppro"> <a href="doing" class="a2">Pendiente</a> </p> </div>
-    <!--|==========| Completado | ↓ |==========|-->
-    <div class="third"> <p class="ppro"> <a href="done" class="a3">Completado</a> </p> </div>
+    <h3 id="greeting">Administrador</h3>
+
+    <div class="row"><!--|==========| Div | Row I | ↓ |==========|-->
+      <div class="col"> <p class="ppro"> <a href="{{ url('/c') }}" class="a1">Carrera</a> </p> </div>
+      <div class="col"> <p class="ppro"> <a href="{{ url('/ch') }}" class="a2">Chatbots</a> </p> </div>
+      <div class="col"> <p class="ppro"> <a href="{{ url('/e') }}" class="a2">Estado</a> </p> </div>
+    <!--|==========| Div | Row I | ↑ |==========|--></div>
+
+    <div class="row"><!--|==========| Div | Row II | ↓ |==========|-->
+      <div class="col"> <p class="ppro"> <a href="{{ url('/f') }}" class="a2">Facultad</a> </p> </div>
+      <div class="col"> <p class="ppro"> <a href="{{ url('/g') }}" class="a1">Gestión</a> </p> </div>
+      <div class="col"> <p class="ppro"> <a href="{{ url('/gt') }}" class="a2">Gestión Tipo</a> </p> </div>
+    <!--|==========| Div | Row II | ↑ |==========|--></div>
+
+    <div class="row"><!--|==========| Div | Row II | ↓ |==========|-->
+      <div class="col"> <p class="ppro"> <a href="{{ url('/h') }}" class="a2">Historial</a> </p> </div>
+      <div class="col"> <p class="ppro"> <a href="{{ url('/n') }}" class="a2">Notas</a> </p> </div>
+      <div class="col"> <p class="ppro"> <a href="{{ url('/c') }}" class="a1">Prioridad</a> </p> </div>
+    <!--|==========| Div | Row II | ↑ |==========|--></div>
+
+    <div class="row"><!--|==========| Div | Row III | ↓ |==========|-->
+      <div class="col"> <p class="ppro"> <a href="{{ url('/r') }}" class="a2">Roles</a> </p> </div>
+      <div class="col"> <p class="ppro"> <a href="{{ url('/s') }}" class="a1">Soporte</a> </p> </div>
+      <div class="col"> <p class="ppro"> <a href="{{ url('/st') }}" class="a2">Soporte Tipo</a> </p> </div>
+      
+    <!--|==========| Div | Row III | ↑ |==========|--></div>
+
+    <div class="row"><!--|==========| Div | Row IV | ↓ |==========|-->
+      <div class="col"> <p class="ppro"> <a href="{{ url('/t') }}" class="a1">Ticket</a> </p> </div>
+      <div class="col"> <p class="ppro"> <a href="{{ url('/u') }}" class="a1">Usuario</a> </p> </div>
+      <div class="col"> <p class="ppro"> <a href="{{ url('/ut') }}" class="a2">Usuario Tipo</a> </p> </div>
+    <!--|==========| Div | Row IV | ↑ |==========|--></div>
+
   <!--|==========| Container | fin | ← | ↑ |==========|--></div>
 
   <!--|========| Modal - CerrarSesion |inicio| ↓ |========|-->
