89eadccb48bfb8ad3c04a1550d7aebb46273ee3e
Gabriela Flores <36106585+gabriella13@users.noreply.github.com>
2021-08-03 18:33:56 -0600

    Add files via upload

--- app/Http/Controllers/GestionController.php
+++ app/Http/Controllers/GestionController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class GestionController extends Controller
 {
@@ -13,7 +15,8 @@ class GestionController extends Controller
      */
     public function index()
     {
-        //
+        $gestion1 = DB::table('gestion')->get();
+        return view('gestion')->with('gestion1',$gestion1);
     }
 
     /**
@@ -23,7 +26,25 @@ class GestionController extends Controller
      */
     public function create()
     {
-        //
+        if (isset($_POST['btnEnviarGestion'])) {
+
+            $NombreGestion = $_POST['txtNombreGestion'];
+            $Creado = date("Y-m-d H:i:s");
+            $Actual = date("Y-m-d H:i:s");
+
+            DB::INSERT("INSERT INTO gestion (gestion_name, created_at, updated_at) VALUES(?,?,?)",[$NombreGestion,$Creado,$Actual]);
+
+            echo '<script language="javascript">';
+            echo 'alert("Datos ingresados correctamente")';
+            echo '</script>';
+            return redirect("/g");
+        
+        } else {
+            echo '<script language="javascript">';
+            echo 'alert("Hubo un error, favor intentarlo de nuevo")';
+            echo '</script>';
+            return redirect("/gestionnuevo");
+        }
     }
 
     /**
@@ -45,7 +66,21 @@ class GestionController extends Controller
      */
     public function show($id)
     {
-        //
+        $gestion_show = DB::SELECT('SELECT * FROM gestion WHERE gestionid = ?',[$id]);
+        if ($gestion_show == null) {
+            echo "<script>
+                  alert('El registro ingresado no fue encontrado, favor seleccionar un registro que exista');
+                  window.location.href='/g';
+                  </script>";
+        } else{
+            foreach($gestion_show as $gestion_queri){
+                $id = $gestion_queri->gestionid;
+                $name = $gestion_queri->gestion_name;
+                $creado = $gestion_queri->created_at;
+                $modificado = $gestion_queri->updated_at;
+                return view('/gestioninfo')->with('id',$id)->with('name',$name)->with('creado',$creado)->with('modificado',$modificado);
+            }
+        }
     }
 
     /**
@@ -56,7 +91,20 @@ class GestionController extends Controller
      */
     public function edit($id)
     {
-        //
+        $gestion_edit = DB::SELECT('SELECT * FROM gestion WHERE gestionid = ?',[$id]);
+
+        if ($gestion_edit == null) {
+            echo "<script>
+                  alert('El registro ingresado no fue encontrado, favor seleccionar un registro que exista');
+                  window.location.href='/g';
+                  </script>";
+        } else{
+            foreach($gestion_edit as $gestion_query){
+                $ii = $gestion_query->gestionid;
+                $name = $gestion_query->gestion_name;    
+                return view('/gestionedit')->with('ii',$ii)->with('name',$name);
+            }
+        }
     }
 
     /**
@@ -68,7 +116,26 @@ class GestionController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if (isset($_POST['btnActualizarR'])) {
+            $ii = $_REQUEST['ii'];
+            $nuevoNombre = $_POST['txtEditNombre'];
+            $nuevoCambio = date("Y-m-d H:i:s");
+
+            $affected = DB::table('gestion')
+              ->where('gestionid', $ii)
+              ->update(['gestion_name' => $nuevoNombre,'updated_at' => $nuevoCambio]);
+
+            echo "<script>
+                  alert('EXITO: Los datos ya fueron actualizados');
+                  window.location.href='/g';
+                  </script>";
+  
+        } else {
+            echo "<script>
+                  alert('ERROR: favor intentarlo de nuevo');
+                  window.location.href='/g';
+                  </script>";
+        }
     }
 
     /**
--- app/Http/Controllers/GestionTipoController.php
+++ app/Http/Controllers/GestionTipoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GestionTipoController extends Controller
 {
@@ -13,7 +14,8 @@ class GestionTipoController extends Controller
      */
     public function index()
     {
-        //
+        $gestiontipo1 = DB::table('gestiontipo')->get();
+        return view('gestiontipo')->with('gestiontipo1',$gestiontipo1);
     }
 
     /**
@@ -23,7 +25,25 @@ class GestionTipoController extends Controller
      */
     public function create()
     {
-        //
+        if (isset($_POST['btnEnviarGestionTipo'])) {
+
+            $NombreGestionTipo = $_POST['txtNombreGestionTipo'];
+            $Creado = date("Y-m-d H:i:s");
+            $Actual = date("Y-m-d H:i:s");
+
+            DB::INSERT("INSERT INTO gestiontipo (gestiontipo_name, created_at, updated_at) VALUES(?,?,?)",[$NombreGestionTipo,$Creado,$Actual]);
+
+            echo '<script language="javascript">';
+            echo 'alert("Datos ingresados correctamente")';
+            echo '</script>';
+            return redirect("/gt");
+        
+        } else {
+            echo '<script language="javascript">';
+            echo 'alert("Hubo un error, favor intentarlo de nuevo")';
+            echo '</script>';
+            return redirect("/gestiontiponuevo");
+        }
     }
 
     /**
@@ -45,7 +65,21 @@ class GestionTipoController extends Controller
      */
     public function show($id)
     {
-        //
+        $gestiontipo_show = DB::SELECT('SELECT * FROM gestiontipo WHERE gestiontipoid = ?',[$id]);
+        if ($gestiontipo_show == null) {
+            echo "<script>
+                  alert('El registro ingresado no fue encontrado, favor seleccionar un registro que exista');
+                  window.location.href='/gt';
+                  </script>";
+        } else{
+            foreach($gestiontipo_show as $gestiontipo_queri){
+                $id = $gestiontipo_queri->gestiontipoid;
+                $name = $gestiontipo_queri->gestiontipo_name;
+                $creado = $gestiontipo_queri->created_at;
+                $modificado = $gestiontipo_queri->updated_at;
+                return view('/gestiontipoinfo')->with('id',$id)->with('name',$name)->with('creado',$creado)->with('modificado',$modificado);
+            }
+        }
     }
 
     /**
@@ -56,7 +90,20 @@ class GestionTipoController extends Controller
      */
     public function edit($id)
     {
-        //
+        $gestiontipo_edit = DB::SELECT('SELECT * FROM gestiontipo WHERE gestiontipoid = ?',[$id]);
+
+        if ($gestiontipo_edit == null) {
+            echo "<script>
+                  alert('El registro ingresado no fue encontrado, favor seleccionar un registro que exista');
+                  window.location.href='/gt';
+                  </script>";
+        } else{
+            foreach($gestiontipo_edit as $gestiontipo_query){
+                $ii = $gestiontipo_query->gestiontipoid;
+                $name = $gestiontipo_query->gestiontipo_name;    
+                return view('/gestiontipoedit')->with('ii',$ii)->with('name',$name);
+            }
+        }
     }
 
     /**
@@ -68,7 +115,26 @@ class GestionTipoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if (isset($_POST['btnActualizarR'])) {
+            $ii = $_REQUEST['ii'];
+            $nuevoNombre = $_POST['txtEditNombre'];
+            $nuevoCambio = date("Y-m-d H:i:s");
+
+            $affected = DB::table('gestiontipo')
+              ->where('gestiontipoid', $ii)
+              ->update(['gestiontipo_name' => $nuevoNombre,'updated_at' => $nuevoCambio]);
+
+            echo "<script>
+                  alert('EXITO: Los datos ya fueron actualizados');
+                  window.location.href='/gt';
+                  </script>";
+  
+        } else {
+            echo "<script>
+                  alert('ERROR: favor intentarlo de nuevo');
+                  window.location.href='/gt';
+                  </script>";
+        }
     }
 
     /**
