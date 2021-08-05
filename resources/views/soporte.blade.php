a28ae9435e5bb6c900532db6639af05e83b2be73
MelvinErazo <35872974+MelvinErazo@users.noreply.github.com>
2021-08-05 15:38:39 -0600

    05AUG21-Actualización (Vistas SOPORTE)
    Se incluyen las 4 vistas funcionales.

--- resources/views/soporte.blade.php
+++ resources/views/soporte.blade.php
@@ -15,7 +15,6 @@
   <!--|==========| Barra de navegacion | ↓ | inicio |==========|-->
   <div class="topnav" id="myTopnav">
     <a href="home">Inicio</a>
-    <a href="notas">Notas</a>
     <a href="perfil">Perfil</a>
     <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir">Salir</a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
@@ -30,7 +29,7 @@
     <!--|==========| Nuevo | ↓ | titulo |==========|-->
     <div class="middle-pro"> <p>Soporte</p> </div>
     <!--|==========| Boton | ir a derecha |==========|-->
-    <div class="btn-right-pro"> <a href="/usuarionuevo" title="Nuevo" class="aarrooww">+</a> </div>
+    <div class="btn-right-pro"> <a href="/soportenuevo" title="Nuevo" class="aarrooww">+</a> </div>
     <!--|==========| Nuevo | ↓ |==========|-->
     
     <!--|==========| Tabla Usuarios | ↓ | inicio |==========|-->
@@ -40,15 +39,15 @@
         <th>Nombre</th>
         <th>Apellido</th>
         <th>Correo</th>
-        <th colspan="2">Opciones</th>
+        <th colspan="3">Opciones</th>
       </thead>
       @php
       $num=1;
       foreach ($soport as $sopor) {
         $id = $sopor->soportecif;
-        $sopnombre = $sopor->soporte_name;
-        $sopapellido = $sopor->soporte_apellido;
-        $sopcorreo = $sopor->soporte_correo;
+        $sopnombre = $sopor->snombre;
+        $sopapellido = $sopor->sapellido;
+        $sopcorreo = $sopor->smail;
       @endphp
       <tbody><td>@php echo $num++; @endphp</td>
         <td>@php echo $sopnombre; @endphp</td>
@@ -63,21 +62,21 @@
   </div>
   <!--|==========| Container | fin | ↑ |==========|-->
 
-  <!--|========| Modal - CerrarSesion |inicio| ↓ |========|-->
+  <!--|========| New Modal - CerrarSesion |inicio| ↓ |========|-->
   <div class="modal fade" id="CerrarSesion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="staticBackdropLabel">Cerrar Sesion</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <a class="modal-btn-closee" data-bs-dismiss="modal" aria-label="Close">X</a>
         </div>
           <div class="modal-body"> ¿Desea salir de la plataforma? </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-          <a href="welcome" type="button" class="btn btn-primary">Si</a>
+            <a class="modal-btn-cerrar" data-bs-dismiss="modal">No</a>
+            <a href="logout" type="button" class="modal-btn-cerrar">Si</a>
         </div>
       </div>
     </div>
-  </div><!--|======| Modal - CerrarSesion |fin| ↑ |======|--> 
+  </div><!--|======| New Modal - CerrarSesion |fin| ↑ |======|--> 
 </body>
 </html>
--- resources/views/soportenuevo.blade.php
+++ resources/views/soportenuevo.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-  <title>Nuevo Usuario</title>
+  <title>Nuevo Soporte</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -15,7 +15,6 @@
   <!--|==========| Barra de navegacion | ↓ | inicio |==========|-->
   <div class="topnav" id="myTopnav">
     <a href="home" class="active">Inicio</a>
-    <a href="notas">Notas</a>
     <a href="perfil">Perfil</a>
     <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir">Salir</a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
@@ -25,49 +24,82 @@
 
   <!--|====| Container | ↓ | → | inicio |====|--><div class="container">
     <!--|==========| Bienvenida | ↓ |==========|-->
-    <h1 id="greeting">Nuevo Usuario</h1>
+    <h2 id="greeting">Soporte Nuevo</h2>
     <!--|==========| Formulario | ↓ | inicio |==========|-->
-    <form action="{{url('/u/create')}}" name="frmUsuarioCreate" method="post">
+    <form action="{{url('/s/create')}}" name="frmUsuarioCreate" method="post">
       @csrf
-      <label class="lblformuser">Nombre</label>
-      <input type="text" class="form-control form-control-lg" name="txtNombre" autocomplete="off" required>
-      <label class="lblformuser">Apellido</label>
-      <input type="text" class="form-control form-control-lg" name="txtApellido" autocomplete="off" required>
-      <label class="lblformuser">Correo</label>
-      <input type="mail" class="form-control form-control-lg" name="txtCorreo" autocomplete="off" required>
-      <label class="lblformuser">Contraseña</label>
-      <input type="password" class="form-control form-control-lg" name="txtPassword" autocomplete="off" required>
-      
-      <label class="lblformuser">Tipo</label>
-      <select name="selTipoUsuario" class="form-control form-control-lg" aria-label="Default select example">
-      @php
-      foreach ($usertypee as $selusertipo) {
-        $tiponame = $selusertipo->soportetipo_name;
-      @endphp
-        <option value="">@php echo $tiponame; @endphp</option>
-      @php } @endphp
-      </select>
-      <div class="d-grid gap-2">
-        <input type="submit" class="btn btn-primary btn-lg" name="btnEnviar" value="Enviar">
-      </div>
+      <div class="row"><!--|==========| Div | Row I | ↓ |==========|-->
+        <div class="col-6">
+          <label class="lblformuser">Nombre</label>
+          <input type="text" class="form-control" name="txtNombre"  autocomplete="off" required>
+        </div>
+        <div class="col-6">
+          <label class="lblformuser">Apellido</label>
+          <input type="text" class="form-control" name="txtApellido" autocomplete="off" required>
+      </div><!--|==========| Div | Row I | ↑ |==========|--></div>
+
+      <div class="row"><!--|==========| Div | Row II | ↓ |==========|-->
+        <div class="col-6">
+          <label class="lblformuser">Correo</label>
+          <input type="email" class="form-control form-control-lg" name="txtCorreo" autocomplete="off" required>
+        </div>
+        <div class="col-6">
+          <label class="lblformuser">Contraseña</label>
+          <input type="password" class="form-control form-control-lg" name="txtPassword" autocomplete="off" required>
+      </div><!--|==========| Div | Row I | ↑ |==========|--></div>
+
+      <div class="row"><!--|==========| Div | Row III | ↓ |==========|-->
+        <div class="col-6">
+          <!--|==========| Select Tipo | ↓ | inicio |==========|-->
+          <label class="lblformuser">Tipo</label>
+          <select name="selTipoSoporte" class="form-control form-control-lg" aria-label="Default select example">
+          @php foreach ($sopotypee as $selsoptipo) {
+                 $tipoid = $selsoptipo->soportetipoid;
+                $tiponame = $selsoptipo->soportetipo_name;
+          @endphp
+            <option value="@php echo $tipoid; @endphp">@php echo $tiponame; @endphp</option>
+          @php } @endphp
+          </select><!--|==========| Select Tipo | ↑ | fin |==========|--></div>
+        <div class="col-6">
+          <!--|==========| Select Rol | ↓ | inicio |==========|-->
+          <label class="lblformuser">Rol</label>
+          <select name="selRoles" class="form-control form-control-lg" aria-label="Default select example">
+          @php  foreach ($rolee as $roless) {
+                  $rolid = $roless->rolesid;
+                  $rolname = $roless->roles_name;
+          @endphp
+            <option value="@php echo $rolid; @endphp">@php echo $rolname; @endphp</option>
+          @php } @endphp
+          </select><!--|==========| Select Tipo | ↑ | fin |==========|--></div>
+      <!--|==========| Div | Row I | ↑ |==========|--></div>
+
+      <div class="row"><!--|==========| Div | Row III | ↓ |==========|-->
+      <div class="col-6">
+      <label class="lblformuser">Telefono</label>
+          <input type="number" class="form-control" name="txtTelefono" autocomplete="off" required>
+      </div>  
+      <div class="col-6">
+          <input type="submit" class="btn-enviar-form" name="btnCrearSoporte" value="Enviar">
+      </div><!--|==========| Div | Row III | ↑ |==========|--></div>
+
     </form><!--|==========| Formulario | ↑ | fin |==========|-->
   <!--|==========| Container | fin | ← | ↑ |==========|--></div>
 
-  <!--|========| Modal - CerrarSesion |inicio| ↓ |========|-->
+  <!--|========| New Modal - CerrarSesion |inicio| ↓ |========|-->
   <div class="modal fade" id="CerrarSesion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="staticBackdropLabel">Cerrar Sesion</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <a class="modal-btn-closee" data-bs-dismiss="modal" aria-label="Close">X</a>
         </div>
           <div class="modal-body"> ¿Desea salir de la plataforma? </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-          <a href="welcome" type="button" class="btn btn-primary">Si</a>
+            <a class="modal-btn-cerrar" data-bs-dismiss="modal">No</a>
+            <a href="logout" type="button" class="modal-btn-cerrar">Si</a>
         </div>
       </div>
     </div>
-  </div><!--|======| Modal - CerrarSesion |fin| ↑ |======|--> 
+  </div><!--|======| New Modal - CerrarSesion |fin| ↑ |======|--> 
 </body>
 </html>
