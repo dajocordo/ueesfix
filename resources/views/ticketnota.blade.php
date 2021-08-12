d7490156e367425fba05720603c1d8e00857e73f
MelvinErazo <35872974+MelvinErazo@users.noreply.github.com>
2021-08-12 02:33:18 -0600

    12AUG21 Actualizacion (VISTAS Ticket Usuario)
    se añaden las vistas de ticket de usuario informacion de usuario y notas en proceso

---
+++ resources/views/ticketnota.blade.php
@@ -0,0 +1,74 @@
+@php
+  session_start();
+  if(isset($_SESSION['student'])){
+@endphp
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <title>Editar Usuario</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="/js/barra.js"></script>
+  <link rel="stylesheet" type="text/css" href="/css/estail.css"> 
+</head>
+<body>
+  <!--|==========| Barra de navegacion | ↓ | inicio |==========|-->
+  <div class="topnav" id="myTopnav">
+    <a href="home" class="active">Inicio</a>
+    <a href="perfil">Perfil</a>
+    <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir">Salir</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+  </div> <!--|==========| Barra de navegacion | ↑ | fin |==========|-->
+
+  <!-- {{ $id=3 }} -->
+
+  <!--|====| Container | ↓ | → | inicio |====|--><div class="container">
+    <!--|==========| Bienvenida | ↓ |==========|-->
+    <h1 id="greeting">Agregar Nota</h1>
+    
+    <!--|==========| Formulario | ↓ | inicio |==========|-->
+    <form action="{{url('/not/create/')}}" name="frmSoporteTipoEdit" method="post">
+      @csrf
+      <input type="hidden" class="form-control form-control-lg" name="ii" value="" autocomplete="off" required>
+      <label class="lblformuser">Usuario</label>
+      <input type="text" class="form-control form-control-lg" name="txtEditNombreSoporteTipo" value="" autocomplete="off" required>
+      <div class="d-grid gap-2">
+      </div>
+      <label class="lblformuser">Nota</label>
+      <input type="text" class="form-control form-control-lg" name="txtEditNombreSoporteTipo" value="" autocomplete="off" required>
+      <div class="d-grid gap-2">
+        <input type="submit" class="btn btn-primary btn-lg" name="btnActualizarSopoTipo" value="Enviar">
+      </div>
+    </form><!--|==========| Formulario | ↑ | fin |==========|-->
+  <!--|==========| Container | fin | ← | ↑ |==========|--></div>
+
+<!--|========| New Modal - CerrarSesion |inicio| ↓ |========|-->
+<div class="modal fade" id="CerrarSesion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Cerrar Sesion</h5>
+          <a class="modal-btn-closee" data-bs-dismiss="modal" aria-label="Close">X</a>
+        </div>
+          <div class="modal-body"> ¿Desea salir de la plataforma? </div>
+        <div class="modal-footer">
+            <a class="modal-btn-cerrar" data-bs-dismiss="modal">No</a>
+            <a href="logout" type="button" class="modal-btn-cerrar">Si</a>
+        </div>
+      </div>
+    </div>
+  </div><!--|======| New Modal - CerrarSesion |fin| ↑ |======|--> 
+</body>
+</html>
+@php  } else{
+      echo "<script>
+            alert('Debes iniciar sesión primero');
+            window.location.href='/index';
+          </script>";
+}  @endphp
