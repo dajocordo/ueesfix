050720b48e4c0e5ebb120e8a634cbb33077ef134
dajocordo <42018108+dajocordo@users.noreply.github.com>
2021-08-13 01:16:39 -0600

    13AUG - KANBAN (Look Gaby)
    KANBAN (Look Gaby)

---
+++ resources/views/tktnuevo.blade.php
@@ -0,0 +1,102 @@
+@php
+  session_start();
+  if(isset($_SESSION['admin'])){
+@endphp
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <title>Ticket Nuevo</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="/js/barra.js"></script>
+  <link rel="stylesheet" type="text/css" href="/css/estail.css">    
+</head>
+<body>
+  <!--|==========| Barra de navegacion | ↓ | inicio |==========|-->
+  <div class="topnav" id="myTopnav">
+    <a href="home">Inicio</a>
+    <a href="perfil">Perfil</a>
+    <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir">Salir</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+  </div><!--|=========| Barra de navegacion | ← | fin |=========|-->
+
+  <!--|==========| Container | ↓ | inicio |==========|-->
+  <div class="container">
+    <!--|==========| Boton | ir a izquierda |==========|-->
+    <div class="btn-left-pro"> <a href="{{ url('/home') }}" title="Inicio" class="aarrooww"><</a> </div>
+    <!--|==========| Tickets | ↓ | titulo |==========|-->
+    <div class="middle-pro"> <p>Nuevo</p> </div>
+    <!--|==========| Boton | ir a derecha |==========|-->
+    <div class="btn-right-pro"> <a href="{{ url('/ticketnuevo') }}" title="Nuevo" class="aarrooww">></a></div>
+
+    <!--|==========| Tabla Ticket (nuevo) | ↓ | inicio |==========|-->
+    <table class="table table-bordered">
+      <thead>
+        <th>Ticket</th>
+        <th>Estado</th>
+        <th>Titulo</th>
+        <th>Modificado</th>
+        <th colspan="3">Opciones</th>
+      </thead>
+      @php // FOREACH TABLA TICKET
+            foreach ($tickeetnuevo as $tickeetnuevoo) {
+              $id = $tickeetnuevoo->ticketid;
+              $titulo = $tickeetnuevoo->ticket_titulo;
+              $estadoid = $tickeetnuevoo->estadolid;
+              $fecha = $tickeetnuevoo->updated_at;
+              $testadoo_show = DB::SELECT('SELECT * FROM estado WHERE estadoid = ?',[$estadoid]);
+
+                // FOREACH TABLA ESTADO
+                foreach($testadoo_show as $testadoo_queri){
+                $estadoid = $testadoo_queri->estadoid;
+                $state = 1;
+                $estado = $testadoo_queri->estado_name;
+
+                if($estadoid == $state){
+      @endphp
+      <tbody>
+        <td>@php echo $id; @endphp</td>
+        <td class="table-primary">@php echo $estado; @endphp</td>
+        <td>@php echo $titulo; @endphp</td>
+        <td>@php echo $fecha; @endphp</td>
+        <td><a class="optionsu" href="/t/@php echo $id; @endphp/edit"><img src="img/edit.png"></a></td>
+        <td><a class="optionsu" href="/t/@php echo $id; @endphp"><img src="img/info.png"></a></td> 
+        <td><a class="optionsu" href="/t/delete"><p class="btndelete">X</p></a></td>
+      </tbody>  
+     @php       }
+              }
+            } 
+      @endphp  
+    </table><!--|==========| Tabla Ticket (nuevo) | ↑ | fin |==========|-->
+  <!--|==========| Container | fin | ↑ |==========|--></div>
+  
+  <!--|========| New Modal - CerrarSesion |inicio| ↓ |========|-->
+  <div class="modal fade" id="CerrarSesion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Cerrar Sesion</h5>
+          <a class="modal-btn-closee" data-bs-dismiss="modal" aria-label="Close">X</a>
+        </div>
+          <div class="modal-body"> ¿Desea salir de la plataforma? </div>
+        <div class="modal-footer">
+            <a class="modal-btn-cerrar" data-bs-dismiss="modal">No</a>
+            <a href="logout" type="button" class="modal-btn-cerrar">Si</a>
+        </div>
+      </div>
+    </div>
+  </div><!--|======| New Modal - CerrarSesion |fin| ↑ |======|--> 
+</body>
+</html>
+@php  } else{
+      echo "<script>
+            alert('Debes iniciar sesión primero');
+            window.location.href='/index';
+          </script>";
+}  @endphp
