4915245462eaf49610958ab2a22a5db987c10ac4
dajocordo <42018108+dajocordo@users.noreply.github.com>
2021-08-13 22:35:10 -0600

    13AUG - Adicción de archivos (password).
    Adicción de archivos (password).

---
+++ passwordchange.blade.php
@@ -0,0 +1,70 @@
+@php
+  session_start();
+  if(isset($_SESSION['student'])){
+@endphp
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <title>Cambiar Contraseña</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="/js/barra.js"></script>
+  <link rel="stylesheet" type="text/css" href="/css/usuariostyle.css"> 
+</head>
+<body>
+  <!--|==========| Barra de navegacion | ↓ | inicio |==========|-->
+  <div class="topnav" id="myTopnav">
+    <a href="inicio">Inicio</a>
+    <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir">Salir</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+  </div> <!--|==========| Barra de navegacion | ↑ | fin |==========|-->
+
+  <!--|====| Container | ↓ | → | inicio |====|--><div class="container">
+    <!--|==========| Boton | Regresar |==========|-->
+    <div class="btn-left-pro"> <a href="{{ url('/s') }}" title="Regresar" class="aarrooww"><</a> </div>
+    <!--|==========| Rol | ↓ | titulo |==========|-->
+    <div class="middle-pro"> <p><img src="/img/add.png"> Soporte [ nuevo ]</p> </div>
+    <!--|==========| Formulario | ↓ | inicio |==========|-->
+    <form action="{{url('/s/create')}}" name="frmUsuarioCreate" method="post">
+      @csrf
+        <label class="lblformuser">Contraseña Actual</label>
+        <input type="password" class="form-control form-control-lg" name="txtPassword" autocomplete="off" required>      
+        <label class="lblformuser">Nueva Contraseña</label>
+        <input type="password" class="form-control form-control-lg" name="txtPassword" autocomplete="off" required>
+        
+        <label class="lblformuser">Repite nueva Contraseña</label>
+        <input type="password" class="form-control form-control-lg" name="txtPassword" autocomplete="off" required>
+        <input type="submit" class="btn-enviar-form" name="btnCrearSoporte" value="Enviar">
+    </form><!--|==========| Formulario | ↑ | fin |==========|-->
+  <!--|==========| Container | fin | ← | ↑ |==========|--></div>
+
+  <!--|========| New Modal - CerrarSesion |inicio| ↓ |========|-->
+  <div class="modal fade" id="CerrarSesion" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Cerrar Sesion</h5>
+          <a class="modal-btn-closee" data-bs-dismiss="modal" aria-label="Close">X</a>
+        </div>
+          <div class="modal-body"> ¿Desea salir de la plataforma? </div>
+        <div class="modal-footer">
+            <a class="modal-btn-cerrar" data-bs-dismiss="modal">No</a>
+            <a href="logout" type="button" class="modal-btn-cerrar">Si</a>
+        </div>
+      </div>
+    </div>
+  </div><!--|======| New Modal - CerrarSesion |fin| ↑ |======|--> 
+</body>
+</html>
+@php  } else{
+      echo "<script>
+            alert('Debes iniciar sesión primero');
+            window.location.href='/index';
+          </script>";
+}  @endphp
