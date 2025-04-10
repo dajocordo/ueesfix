66fda3f3cc694533299b3ec6f30bbfb00837adda
dj <dajcordova@gmail.com>
2025-04-10 08:37:19 -0600

    se optimiza codigo, moviendo la topnav que es comun en un archivo como helper

---
+++ resources/views/tool/topnav.blade.php
@@ -0,0 +1,8 @@
+<div class="topnav" id="myTopnav">
+    <a href="home">Inicio</a>
+    <a href="perfil">Perfil</a>
+    <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir">Salir</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+</div>
--- resources/views/usuarionuevo.blade.php
+++ resources/views/usuarionuevo.blade.php
@@ -12,15 +12,7 @@
   <link rel="stylesheet" type="text/css" href="/css/estail.css"> 
 </head>
 <body>
-  <!--|==========| Barra de navegacion | ↓ | inicio |==========|-->
-  <div class="topnav" id="myTopnav">
-    <a href="home">Inicio</a>
-    <a href="perfil">Perfil</a>
-    <a href="#CerrarSesion" data-bs-toggle="modal" title="Salir">Salir</a>
-    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-      <i class="fa fa-bars"></i>
-    </a>
-  </div> <!--|==========| Barra de navegacion | ↑ | fin |==========|-->
+ @include('tool.topnav')
 
   <!--|====| Container | ↓ | → | inicio |====|--><div class="container">
     <!--|==========| Boton | Regresar |==========|-->
