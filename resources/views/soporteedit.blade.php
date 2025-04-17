a9c874259382201bf014d3963032f21ec4918220
dj <dajcordova@gmail.com>
2025-04-16 22:13:06 -0600

    new form fields for support edit

--- resources/views/soporteedit.blade.php
+++ resources/views/soporteedit.blade.php
@@ -1,38 +1,25 @@
 {{--| admin |--}}
 
 @extends('building')
-
 @section('title', 'Editar soporte')
-
 @section('content')
 
-<body>
- @include('tool.topnav')
-
-  <!-- {{ $id=3 }} -->
-
-  <!--|====| Container | ↓ | → | inicio |====|-->
-<div class="container">
-    <!--|==========| Bienvenida | ↓ |==========|-->
-    <h1 id="greeting">Editar Soporte</h1>
-    
-    <!--|==========| Formulario | ↓ | inicio |==========|-->
+  @include('tool.topnav')
+  <div class="container">
+    <h1 id="greeting">Editar Soporte</h1>  
     <form action="{{url('/s/update/')}}" method="post">
       @csrf
-      <input type="hidden" class="form-control form-control-lg" name="ii" value="@php echo $ii; @endphp" autocomplete="off" required>
+      <input type="hidden" class="form-control form-control-lg" name="ii" value="{{ $user['id'] }}" autocomplete="off" required>
       <label class="lblformuser">Nombre</label>
-      <input type="text" class="form-control form-control-lg" name="txtEditNombre" value="@php echo $name; @endphp" autocomplete="off" required>
-      <label class="lblformuser">Apellido</label>
-      <input type="text" class="form-control form-control-lg" name="txtEditApellido" value="@php echo $apellido; @endphp" autocomplete="off" required>
+      <input type="text" class="form-control form-control-lg" name="txtEditNombre" value="{{ $user['name'] }}" autocomplete="off" required>
       <label class="lblformuser">Correo</label>
-      <input type="email" class="form-control form-control-lg" name="txtEditCorreo" value="@php echo $correo; @endphp" autocomplete="off" required>
-      <label class="lblformuser">Telefono</label>
-      <input type="number" class="form-control form-control-lg" name="txtEditTelefono" value="@php echo $telefono; @endphp" autocomplete="off" required>     
+      <input type="email" class="form-control form-control-lg" name="txtEditCorreo" value="{{ $user['correo'] }}" autocomplete="off" required>
+      {{-- <label class="lblformuser">Telefono</label>
+      <input type="number" class="form-control form-control-lg" name="txtEditTelefono" value="{{ $user['telefono'] }}" autocomplete="off" required>      --}}
       <div class="d-grid gap-2">
         <input type="submit" class="btn btn-primary btn-lg" name="btnActualizarSoporte" value="Actualizar">
       </div>
-    </form><!--|==========| Formulario | ↑ | fin |==========|-->
-  <!--|==========| Container | fin | ← | ↑ |==========|-->
-</div>
+    </form>
+  </div>
 
 @endsection
