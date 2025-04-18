8dcb3fbad2fc94715b87cb5a1a56320ea988fa3d
dj <dajcordova@gmail.com>
2025-04-17 22:45:31 -0600

    new lists for the new ticket form

--- app/Http/Controllers/PrioridadController.php
+++ app/Http/Controllers/PrioridadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listado;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -14,8 +15,9 @@ class PrioridadController extends Controller
      */
     public function index()
     {
-        $priori = DB::table('prioridad')->get();
-        return view('prioridad')->with('priori',$priori);
+        $getPriori = Listado::where('grupo', 'prioridad')->get();
+        $priori = formatLists($getPriori);
+        return view('prioridad')->with('priori', $priori);
     }
 
     /**
