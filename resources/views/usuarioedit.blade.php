0fa4c9231cbbdda9ebcc818d6cc836266f84839a
dj <dajcordova@gmail.com>
2025-04-19 17:11:25 -0600

    fix user info

--- resources/views/usuarioedit.blade.php
+++ resources/views/usuarioedit.blade.php
@@ -14,13 +14,13 @@
     
     <form action="{{url('/u/update/')}}" method="post">
       @csrf
-      <input type="hidden" class="form-control form-control-lg" name="ii" value="{{ $user['id'] }}" autocomplete="off" required>
+      <input type="hidden" class="form-control form-control-lg" name="ii" value="{{ $user->id }}" autocomplete="off" required>
       <label class="lblformuser">Nombre</label>
-      <input type="text" class="form-control form-control-lg" name="txtEditNombre" value="{{ $user['name'] }}" autocomplete="off" required>
+      <input type="text" class="form-control form-control-lg" name="txtEditNombre" value="{{ $user->name }}" autocomplete="off" required>
       {{-- <label class="lblformuser">Apellido</label>
-      <input type="text" class="form-control form-control-lg" name="txtEditApellido" value="{{ $user['apellido'] }}" autocomplete="off" required> --}}
+      <input type="text" class="form-control form-control-lg" name="txtEditApellido" value="{{ $user->apellido }}" autocomplete="off" required> --}}
       <label class="lblformuser">Correo</label>
-      <input type="mail" class="form-control form-control-lg" name="txtEditCorreo" value="{{ $user['correo'] }}" autocomplete="off" required>
+      <input type="mail" class="form-control form-control-lg" name="txtEditCorreo" value="{{ $user->email }}" autocomplete="off" required>
       <div class="d-grid gap-2">
         <input type="submit" class="btn btn-primary btn-lg" name="btnActualizarU" value="Actualizar">
       </div>
