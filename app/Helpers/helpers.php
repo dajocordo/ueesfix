36ec7fc174202abdf84183831fbaf7b4df090d0a
dj <dajcordova@gmail.com>
2025-04-21 20:21:45 -0600

    new JsonResponse functions to helper, both, the success and error response

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,5 +1,29 @@
 <?php
 
+use Illuminate\Http\JsonResponse;
+
+if (!function_exists('responseOK')) {
+    function responseOK($data = [], $status = 200, $message = ""): JsonResponse
+    {
+        $respuesta = [
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ];
+        return response()->json($respuesta, $status);
+    }
+}
+
+if (!function_exists('responseError')) {
+    function responseError($message = "", $status = 400): JsonResponse
+    {
+        $respuesta = [
+            'message' => $message
+        ];
+        return response()->json($respuesta, $status);
+    }
+}
+
 if (!function_exists('formatUsers')) {
     function formatUsers($usuarios): array
     {
@@ -35,11 +59,29 @@ if (!function_exists('formatLists')) {
                     $lt->origen = $listado->origen->valor ?? '';
                 }
                 $lt->fecha = formatDate($listado->created_at);
+                $lt->creado = formatDate($listado->created_at);
                 $lt->actualizado = formatDate($listado->updated_at);
                 $list[] = $lt;
             }
         }
         return $list;
+    }
+}
+
+if (!function_exists('formatOneListado')) {
+    function formatOneListado($listado): object
+    {
+        $info = new \stdClass();
+        $info->id = $listado->id ?? '';
+        $info->name = $listado->valor ?? '';
+        $info->origen = '';
+        if ($listado->id_origin) {
+            $info->origen = $listado->origen->valor ?? '';
+        }
+        $info->fecha = formatDate($listado->created_at);
+        $info->creado = formatDate($listado->created_at);
+        $info->actualizado = formatDate($listado->updated_at);
+        return $info;
     }
 }
 
