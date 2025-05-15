f68c8a33023849876ef163ae90b40efe9ab99674
dj <dajcordova@gmail.com>
2025-05-14 21:34:16 -0600

    fix gestion routes

--- routes/gestion.php
+++ routes/gestion.php
@@ -3,18 +3,21 @@
 use App\Http\Controllers\GestionController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('gestion')->prefix('gestion')->group(function() {
+Route::name('gestion')->prefix('gestion')->name('gestion.')->group(function() {
     
     Route::get('/', [GestionController::class, 'index']);
     Route::get('/{id}', [GestionController::class, 'show']);
 
-    
     //  GESTION
-    Route::get('/gestionnuevo', function() {return view('gestionnuevo');});
-    Route::resource('/g', GestionController::class);
+    // Route::get('/new', [GestionController::class, 'newgestion'])->name('nuevo');
+    // Route::get('/new', function() {return view('gestionnuevo');})->name('nuevo');
+    
     Route::get('/{id}/edit', [GestionController::class, 'edit']);
+    
     Route::post('/create', [GestionController::class, 'store']);
     Route::post('/update', [GestionController::class, 'update']);
-    Route::get('gtn', [GestionController::class, 'newgestion']);
-
+    
+    
 });
+
+Route::get('gtn', [GestionController::class, 'newgestion']);
